625d1889a7510c69f038c2a846b017a83bd0a0fa
See Loo Jane <muigek34@yahoo.com.sg>
2017-08-24 01:17:35 +0800

    Create first version of review model

---
+++ src/app/review.ts
@@ -0,0 +1,13 @@
+export class Review {
+    id: number;
+    author: string;
+    comments: string;
+    dateCreated: string;
+    dateLastEdited: string;
+    // Possibly refactor these ratings into a Rating model
+    teachingTeamRating: number; 
+    enjoyabilityRating: number;
+    workloadRating: number;
+    difficultyRating: number;
+    isRecommend: boolean;
+}
