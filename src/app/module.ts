2493f647a7b4e28154368aa6cc3bab2ae48b7e3a
See Soon Kiat <a0124552@u.nus.edu>
2017-08-23 16:54:27 +0800

    Add new mock data and module model

--- src/app/module.ts
+++ src/app/module.ts
@@ -1,4 +1,9 @@
 export class Module {
     id: number;
     code: string;
+    modName: string;
+    modCredits: number;
+    professor: string;
+    lastTaught: string;
+    lastReviewed: string;
 }
