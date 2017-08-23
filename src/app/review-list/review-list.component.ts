a77309e065dfb0b4de2d0f8c8aec9fd9ffe859cb
See Loo Jane <muigek34@yahoo.com.sg>
2017-08-24 01:34:28 +0800

    Add basic code to ReviewListComponent to fetch mock reviews

--- src/app/review-list/review-list.component.ts
+++ src/app/review-list/review-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Review } from '../review';
 
 @Component({
   selector: 'app-review-list',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewListComponent implements OnInit {
 
-  constructor() { }
+  reviews: Review[] = null;
+  searchItem: string = "";
 
-  ngOnInit() {
+  constructor(private reviewService: ReviewService) { }
+
+  ngOnInit(): void {
+    this.reviewService.getReviewsSlowly()
+      .then(reviews => this.reviews = reviews);
   }
 
 }
