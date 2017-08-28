8188ac7973d96d724fb8dfccbfbbbe8618b517c6
KnightNiwrem <merwinwee@gmail.com>
2017-08-29 00:59:19 +0800

    Moved FB Login to a new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,7 @@ import { ReviewListComponent } from './review-list/review-list.component';
 import { ModuleReviewFormComponent } from './module-review-form/module-review-form.component';
 import { PrivacyPageComponent } from './privacy-page/privacy-page.component';
 import { TermsOfServicePageComponent } from './terms-of-service-page/terms-of-service-page.component';
+import { UserLoginComponent } from './user-login/user-login.component';
 
 @NgModule({
   declarations: [
@@ -40,6 +41,7 @@ import { TermsOfServicePageComponent } from './terms-of-service-page/terms-of-se
     ModuleReviewFormComponent,
     PrivacyPageComponent,
     TermsOfServicePageComponent,
+    UserLoginComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/header-bar/header-bar.component.ts
+++ src/app/header-bar/header-bar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavOption } from '../navOption';
-import { FacebookService, InitParams, LoginResponse, LoginOptions } from 'ngx-facebook';
+import { UserLoginComponent } from '../user-login/user-login.component';
 
 @Component({
   selector: 'app-header-bar',
@@ -10,8 +10,6 @@ import { FacebookService, InitParams, LoginResponse, LoginOptions } from 'ngx-fa
 export class HeaderBarComponent implements OnInit {
 
   public appName: string = "NUS REVIEWS";
-  private fbService: FacebookService;
-  public fbProfile: any;
 
   public navOptions: NavOption[] = [
     {
@@ -32,56 +30,7 @@ export class HeaderBarComponent implements OnInit {
     }
   ];
 
-  private options: LoginOptions = {
-    scope: 'public_profile, user_friends, email',
-    return_scopes: true,
-    enable_profile_selector: true
-  };
-
-  constructor(fbService: FacebookService) { 
-    this.fbService = fbService;
-
-    let initParams: InitParams = {
-      appId: '113701052652102',
-      xfbml: true,
-      version: 'v2.8'
-    };
-
-    fbService.init(initParams);
-    (<any>window).fbService = this.fbService;
-    (<any>window).headerComponent = this;
-
-    this.fbService.getLoginStatus().then((res) => {
-      if (res.status === "connected") {
-        this.getFacebookProfile();
-      }
-    });
-  }
-
   ngOnInit() {
-  }
-
-  loginWithFacebook() {
-    this.fbService.login(this.options).then((response: LoginResponse) => {
-      console.log(response);
-    }).catch((error: any) => {
-      console.error(error)
-    });
-  }
-
-  logoutFromFacebook() {
-    this.fbService.logout().then(() => {
-      console.log('Logged out!');
-      this.fbProfile = undefined;
-    });
-  }
-
-  getFacebookProfile() {
-    this.fbService.api('/me').then((res) => {
-      this.fbProfile = res;
-    }).catch((err) => {
-      throw new err;
-    });
   }
 
 }
