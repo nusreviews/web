edb3d86b470652cf6e692efc498bab205924b5e4
See Loo Jane <muigek34@yahoo.com.sg>
2017-08-29 01:34:56 +0800

    Basic rendition of like button

--- src/app/like-button/like-button.component.ts
+++ src/app/like-button/like-button.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LikeButtonComponent implements OnInit {
 
+  isActive: boolean = false;
+  iconName: string = "favorite_border";
+  numLikes: number = 0;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onClick(): void {
+    this.isActive = !this.isActive;
+
+    if (this.isActive) {
+      this.iconName = "favorite"; 
+      this.numLikes++;
+    } else {
+      this.iconName = "favorite_border";
+      this.numLikes--;
+    }
+
   }
 
 }
