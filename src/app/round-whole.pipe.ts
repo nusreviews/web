84e38a4bf45cac8790020a4678df6cd65f9bea48
See Loo Jane <muigek34@yahoo.com.sg>
2017-09-03 15:24:27 +0800

    Update module percent with roundWhole pipe on dashboard page

--- src/app/round-whole.pipe.ts
+++ src/app/round-whole.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class RoundWholePipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform (input: number) {
+    return Math.round(input);
   }
 
 }
