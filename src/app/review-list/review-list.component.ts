9b16ca7f6923921c2598e7f87d3c28a8261395c9
See Loo Jane <muigek34@yahoo.com.sg>
2017-09-03 18:40:52 +0800

    Removed ngAfterViewInit code as it messes up reviews css

--- src/app/review-list/review-list.component.ts
+++ src/app/review-list/review-list.component.ts
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
   templateUrl: './review-list.component.html',
   styleUrls: ['./review-list.component.css']
 })
-export class ReviewListComponent implements OnInit, AfterViewInit {
+export class ReviewListComponent implements OnInit {
 
   reviews: Review[] = null;
   searchItem: string = "";
@@ -23,14 +23,14 @@ export class ReviewListComponent implements OnInit, AfterViewInit {
       .then(reviews => this.reviews = reviews);
   }
 
-  ngAfterViewInit(): void {
-    const tree = this.router.parseUrl(this.router.url);
-    if (tree.fragment) {
-      const frag = tree.fragment;
-      const element = document.getElementById(frag);
-      console.log(element);
-      // Cant seem to find element
-    }
-  }
+  // ngAfterViewInit(): void {
+  //   const tree = this.router.parseUrl(this.router.url);
+  //   if (tree.fragment) {
+  //     const frag = tree.fragment;
+  //     const element = document.getElementById(frag);
+  //     console.log(element);
+  //     // Cant seem to find element
+  //   }
+  // }
 
 }
