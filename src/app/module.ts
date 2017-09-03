8021a63389445908a5e1c0242c49d229477c2e51
See Loo Jane <muigek34@yahoo.com.sg>
2017-09-03 15:17:47 +0800

    Edited attributes for Module, added constructors and deserialiser

--- src/app/module.ts
+++ src/app/module.ts
@@ -2,9 +2,10 @@ export class Module {
     id: number;
     code: string;
     modName: string;
-    modCredits: number;
-    professor: string;
-    lastTaught: string;
+    //modCredits: number;
+    description: string;
+    //professor: string;
+    //lastTaught: string;
     lastReviewed: string;
     overallVerdict: string; // recommend, not-recommend
     percentageRecommend: number;
@@ -12,5 +13,47 @@ export class Module {
     enjoyabilityRating: number;
     difficultyRating: number;
     workloadRating: number;
+
+    constructor(id: number, 
+                modName: string,
+                description: string,
+                lastReviewed: string,
+                percentageRecommend: number,
+                teachingRating: number,
+                enjoyabilityRating: number,
+                difficultyRating: number,
+                workloadRating: number) {
+                    this.id = id;
+                    this.modName = modName;
+                    this.description = description;
+                    this.lastReviewed = lastReviewed;
+                    this.percentageRecommend = percentageRecommend;
+                    this.teachingRating = teachingRating;
+                    this.enjoyabilityRating = enjoyabilityRating;
+                    this.difficultyRating = difficultyRating;
+                    this.workloadRating = workloadRating;
+                }
+    
+    private getOverallVerdict() {
+        if (this.percentageRecommend >= 50) {
+            return 'recommend';
+        } else {
+            return 'not-recommend';
+        }
+    }
+
+    public static deserialiseJson(jsonObject: JSON) {
+        let module = new Module(jsonObject['modId'],
+                                jsonObject['name'],
+                                jsonObject['description'],
+                                jsonObject['dateUpdated'],
+                                jsonObject['percentage'],
+                                jsonObject['avgTeaching'],
+                                jsonObject['avgEnjoyability'],
+                                jsonObject['avgDifficulty'],
+                                jsonObject['avgWorkload']);
+        
+        return module;
+    }
 
 }
