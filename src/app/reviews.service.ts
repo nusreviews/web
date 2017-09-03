7252f88fa63b80e9ced90dd19836be28b0944212
See Loo Jane <muigek34@yahoo.com.sg>
2017-09-03 18:40:31 +0800

    Update reviews service to link with API

--- src/app/reviews.service.ts
+++ src/app/reviews.service.ts
@@ -1,27 +1,75 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
 import { Review } from './review';
 import { REVIEWS } from './mock-reviews';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class ReviewsService {
+	
+	constructor(private http: Http) { }
+	
+	// Retrieves list of reviews
+	// If modId != null, returns a list of reviews for that module
+	// If userId != null, returns a list of reviews, with information on whether
+	// the input userId has liked the reviews or not.
+	getReviews(modId: string, userId: string): Promise<Review[]> {
+		let url = 'https://api.nusreviews.com/getReviews?';
+		
+		if (modId == null) {
+			url = url + '&modId=' + modId;
+		}
+		
+		if (userId == null) {
+			url = url + '&likedBy=' + userId;
+		}
 
-  constructor() { }
+		return this.http.get(url)
+        .toPromise()
+        .then(response => {
+            let jsonArray = response.json()["reviews"];
+            let reviews = jsonArray.map(function(x) {
+                let deserialisedReview = Review.deserialiseJson(x);
+                return deserialisedReview;
+            });
+            //console.log(reviews);
+            return reviews;
+        }) 
+        .catch(this.handleError);
+	}
 
-  getReviews(): Promise<Review[]> {
-    return Promise.resolve(REVIEWS);
-  }
-  getReviewsSlowly(): Promise<Review[]> {
-    return new Promise(resolve => {
-      // Simulate server latency with 1 second delay
-      setTimeout(() => resolve(this.getReviews()), 1000);
-    });
-  }
-  getReviewById(id: number): Promise<Review> {
-      return this.getReviews().then(Reviews => Reviews.find(Review => Review.id === id));
-  }
-  getReviewsByUserId(userId: string): Promise<Review[]> {
-    return this.getReviews().then(reviews => reviews.filter(review => review.userId === userId));
-  }
-  
+	getReviewsSlowly(): Promise<Review[]> {
+		return new Promise(resolve => {
+			// Simulate server latency with 1 second delay
+			setTimeout(() => resolve(this.getReviews('CS1020', null)), 1000);
+		});
+	}
+	
+	// getReviewById(id: string): Promise<Review> {
+	// 	return this.getReviews(id, null).then(Reviews => Reviews.find(Review => Review.id === id));
+	// }
+
+
+	// Return list of reviews written by the input user
+	getReviewsByUserId(userId: string): Promise<Review[]> {
+		return this.http.get('https://api.nusreviews.com/getReviews?user=' + userId)
+        .toPromise()
+        .then(response => {
+            let jsonArray = response.json()["reviews"];
+            let reviews = jsonArray.map(function(x) {
+                let deserialisedReview = Review.deserialiseJson(x);
+                return deserialisedReview;
+            });
+            console.log(reviews);
+            return reviews;
+        }) 
+        .catch(this.handleError);
+	}
+
+	private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
+    }
+	
 }
 
