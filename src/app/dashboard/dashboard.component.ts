3cef8df54d5b9cbed07c0c7cab6785aa93708b57
See Loo Jane <muigek34@yahoo.com.sg>
2017-09-03 22:31:09 +0800

    Remove console log from dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -25,7 +25,6 @@ export class DashboardComponent implements OnInit {
   ngOnInit(): void {
     this.moduleService.getModules()
       .then(modules => {
-        console.log(modules);
         this.modules = modules
       });
   }
