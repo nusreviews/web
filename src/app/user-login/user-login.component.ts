2105868cc10e2e44fda1ea8ed57e6e15cce40e6f
See Soon Kiat <a0124552@u.nus.edu>
2017-09-04 13:26:13 +0800

    Temp changes to user login component

--- src/app/user-login/user-login.component.ts
+++ src/app/user-login/user-login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
 import { LoginService } from '../login.service';
 
 @Component({
@@ -6,9 +6,11 @@ import { LoginService } from '../login.service';
   templateUrl: './user-login.component.html',
   styleUrls: ['./user-login.component.css']
 })
-export class UserLoginComponent implements OnInit {
+export class UserLoginComponent implements OnInit, OnChanges {
 
+  @Input()
   public loginService: LoginService;
+  userProfile = null;
 
   constructor(loginService: LoginService) { 
     this.loginService = loginService;
@@ -16,6 +18,53 @@ export class UserLoginComponent implements OnInit {
 
   ngOnInit() {
     (<any>window).userLogin = this;
+    if (this.loginService.fbProfile) {
+      this.getUserProfile();
+    } else {
+      this.userProfile = null;
+    }
   }
 
+  ngOnChanges() {
+    if (this.loginService.fbProfile) {
+      console.log(this.loginService.fbProfile);
+    } else {
+      console.log("fb profile nullified");
+    }
+  }
+
+  doFacebookLogin(): Promise<void> {
+    return new Promise<void>((resolve, error) => {
+      console.log('toggle');
+      resolve(this.loginService.toggleFacebookLogin());
+    });
+  }
+
+  getUserProfile() {
+    this.loginService.secureApiGet("https://api.nusreviews.com/profile").then((res) => {
+      console.log(res.json());
+      this.userProfile = res.json();
+      console.log(this.userProfile.displayName + " has logged in");
+    }).catch((err) => {
+      console.log("unauthorized");
+      this.loginService._logoutFromFacebook();
+    });    
+  }
+
+  doLogin() {
+    this.doFacebookLogin()
+    .then(x => {
+      console.log('after logged in ' + this.loginService.fbProfile);
+      if(this.loginService.fbProfile) {
+        this.getUserProfile()
+      } else {
+        this.userProfile = null;
+      }
+    })
+    .catch(x => console.log(x));
+  }
+
+  doLogout() {
+    this.loginService._logoutFromFacebook();
+  }
 }
