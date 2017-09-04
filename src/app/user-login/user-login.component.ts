4dc7a2989997fcd586e8455ffb8f020fea198c20
See Soon Kiat <a0124552@u.nus.edu>
2017-09-04 14:19:45 +0800

    Changed user login component to correctly reflect user after logging in

--- src/app/user-login/user-login.component.ts
+++ src/app/user-login/user-login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnChanges, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../login.service';
 
 @Component({
@@ -6,11 +6,9 @@ import { LoginService } from '../login.service';
   templateUrl: './user-login.component.html',
   styleUrls: ['./user-login.component.css']
 })
-export class UserLoginComponent implements OnInit, OnChanges {
+export class UserLoginComponent implements OnInit {
 
-  @Input()
   public loginService: LoginService;
-  userProfile = null;
 
   constructor(loginService: LoginService) { 
     this.loginService = loginService;
@@ -18,53 +16,12 @@ export class UserLoginComponent implements OnInit, OnChanges {
 
   ngOnInit() {
     (<any>window).userLogin = this;
-    if (this.loginService.fbProfile) {
-      this.getUserProfile();
-    } else {
-      this.userProfile = null;
+    if (this.loginService.getProfile()) {
+      console.log(this.loginService.getProfile().nusreviews);
     }
   }
 
-  ngOnChanges() {
-    if (this.loginService.fbProfile) {
-      console.log(this.loginService.fbProfile);
-    } else {
-      console.log("fb profile nullified");
-    }
-  }
-
-  doFacebookLogin(): Promise<void> {
-    return new Promise<void>((resolve, error) => {
-      console.log('toggle');
-      resolve(this.loginService.toggleFacebookLogin());
-    });
-  }
-
-  getUserProfile() {
-    this.loginService.secureApiGet("https://api.nusreviews.com/profile").then((res) => {
-      console.log(res.json());
-      this.userProfile = res.json();
-      console.log(this.userProfile.displayName + " has logged in");
-    }).catch((err) => {
-      console.log("unauthorized");
-      this.loginService._logoutFromFacebook();
-    });    
-  }
-
-  doLogin() {
-    this.doFacebookLogin()
-    .then(x => {
-      console.log('after logged in ' + this.loginService.fbProfile);
-      if(this.loginService.fbProfile) {
-        this.getUserProfile()
-      } else {
-        this.userProfile = null;
-      }
-    })
-    .catch(x => console.log(x));
-  }
-
-  doLogout() {
-    this.loginService._logoutFromFacebook();
+  toggleFacebookLogin() {
+    this.loginService.toggleFacebookLogin();
   }
 }
